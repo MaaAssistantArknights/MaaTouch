c46fdeea0d71ac18f96025183f091543d00f36b3
zzyyyl <aysyxx53@hotmail.com>
2023-10-18 16:40:43 +0800

    feat: 兼容更低的安卓版本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
 
     defaultConfig {
         applicationId = "com.shxyke.MaaTouch"
-        minSdk = 28
-        targetSdk = 34
+        minSdk = 21
+        targetSdk = 31
         versionCode = 1
         versionName = "1.0"
 
