c639ad2fe53d2904fea7926190906e7ed558e021
shxyke <shxyke@gmail.com>
2023-10-18 11:18:58 +0800

    Upgrade project structure

---
+++ app/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.shxyke.MaaTouch"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.shxyke.MaaTouch"
+        minSdk = 28
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar", "*.aar"))))
+    testImplementation("junit:junit:4.13.2")
+}
